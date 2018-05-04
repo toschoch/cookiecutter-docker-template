61f174de64523d2fd7245f64bff98165b9711943
Tobias Schoch <Tobias.Schoch@helbling.ch>
2018-05-04 09:45:52 +0200

    added basic tests

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -1,7 +1,20 @@
 from contextlib import contextmanager
-
+import subprocess
 import os
 
+CCDS_ROOT = os.path.abspath(
+    os.path.join(
+        __file__,
+        os.pardir,
+        os.pardir
+    )
+)
+
+
+def read(filename):
+    with open(filename, 'r') as fp:
+        s = fp.read()
+    return s
 
 @contextmanager
 def inside_dir(dirpath):
@@ -18,13 +31,43 @@ def inside_dir(dirpath):
 
 
 def test_project_tree(cookies):
-    result = cookies.bake(extra_context={'project_slug': 'test_project'})
+    result = cookies.bake(template=CCDS_ROOT, extra_context={'project_slug': 'test_project'})
     assert result.exit_code == 0
     assert result.exception is None
     assert result.project.basename == 'test_project'
+    assert read(result.project.join('README.md'))=="""Python Package
+===============================
+author: Your name
+
+Overview
+--------
+
+A python package that can be installed with pip.
 
 
-def test_run_flake8(cookies):
-    result = cookies.bake(extra_context={'project_slug': 'flake8_compat'})
-    with inside_dir(str(result.project)):
-        assert os.subprocess.check_call(['flake8']) == 0
+Change-Log
+----------
+##### 0.0.1
+* initial version
+
+
+Installation / Usage
+--------------------
+clone the repo:
+
+```
+git clone <git-url>
+```
+build the docker image
+```
+docker build . -t test_project
+```
+
+Example
+-------
+
+run a container of the image
+```
+docker run -v ... -p ... test_project
+```"""
+    assert os.path.exists(result.project.join("Dockerfile"))
